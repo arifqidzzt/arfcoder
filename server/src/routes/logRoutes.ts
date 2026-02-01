deb30f419332bc15ba9044f3c891650e1da71494
Arifqi <arifqidzzt@gmail.com>
2026-02-01 22:04:28 +0700

    Implementasi 2FA, Voucher, Flash Sale, Review, dan Progress Tracker

--- server/src/routes/logRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getLogs } from '../controllers/logController';
-import { authenticate, authorize } from '../middlewares/auth';
-
-const router = Router();
-
-router.get('/', authenticate, authorize(['ADMIN', 'SUPER_ADMIN']), getLogs);
-
-export default router;
